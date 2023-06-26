43419ae2f4f25596567cf34fdbc6eb239589dbb5
Balolebwami <tchasingajacques@gmail.com>
2023-06-26 20:07:00 +0200

    new organization

--- index.js
+++ index.js
@@ -1,162 +1,98 @@
-const leaderboardApi = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
-const gameID = 'vtL6cvMOxCyImcJYZjbt';
+const baseURL = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/";
 
-const gameName = async () => {
-  const response = await fetch(`${leaderboardApi}gameID/`, {
-    method: 'POST',
-    body: JSON.stringify({
-      name: 'Tchasinga jack',
-    }),
-    headers: {
-      'content-type': 'application/json; charset=UTF-8',
-    },
-  });
+let gameId;
 
-  const id = await response.json();
-  return id;
-};
-
-const showingScores = async () => {
-  const receiveData = await fetch(`${leaderboardApi}games/${gameID}/scores/`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  const result = await receiveData.json();
-  generateScores(result.result);
-};
-
-const submitData = async (name, score, gameID) => {
-  const submitData = await fetch(`${leaderboardApi}games/${gameID}/scores/`, {
-    method: 'POST',
-    body: JSON.stringify({
-      user: name,
-      score,
-    }),
-    headers: {
-      'content-type': 'application/json; charset=UTF-8',
-    },
-  });
-
-  const response = await submitData.json();
-  return response;
-};
-
-const generateScores = (score) => {
-  const ul = document.querySelector('.content');
-  ul.innerHTML = '';
-  score.forEach((element) => {
-    const li = document.createElement('li');
-    li.innerHTML = `${element.user} : ${element.score}`;
-    ul.appendChild(li);
-  });
-};
-
-const form = document.getElementById("form");
-const dataContainer = document.getElementById("data-container");
-
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  let Name = document.getElementById("name").value;
-  let Email = document.getElementById("email").value;
-  let Number = document.getElementById("number").value;
-
-  document.getElementById("name").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("number").value = "";
-
-  const url = `${leaderboardApi}games/${gameID}/scores/`;
-  const options = {
-    method: "POST",
-    body: JSON.stringify({
-      user: Name,
-      score: Number,
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  };
+// Create a new game
+async function createGame() {
+  const gameName = "My cool new game"; // Change this to your desired game name
 
   try {
-    const response = await fetch(url, options);
+    const response = await fetch(`${baseURL}games/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: gameName,
+      }),
+    });
+
     const data = await response.json();
-    console.log(data);
-
-    // Retrieve existing saved data from localStorage
-    let existingData = JSON.parse(localStorage.getItem("savedData")) || [];
-
-    // Add newly entered data to existing saved data
-    existingData.push({ name: Name, email: Email, number: Number });
-
-    // Save the updated data in localStorage
-    localStorage.setItem("savedData", JSON.stringify(existingData));
-
-    // Display all saved data on the page
-    displaySavedData(existingData);
+    gameId = data.result.split("ID: ")[1]; // Extract the game ID from the response
   } catch (error) {
-    console.error("Error:", error);
+    console.error("Error creating game:", error);
+  }
+}
+
+// Add a new score
+async function addScore(user, score) {
+  try {
+    const response = await fetch(`${baseURL}games/${gameId}/scores/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: user,
+        score: score,
+      }),
+    });
+
+    const data = await response.json();
+    console.log(data.result); // Log the result from the API
+  } catch (error) {
+    console.error("Error adding score:", error);
+  }
+}
+
+// Get all scores for the game
+async function getScores() {
+  try {
+    const response = await fetch(`${baseURL}games/${gameId}/scores/`);
+    const data = await response.json();
+    displayScores(data.result); // Display the scores on the page
+  } catch (error) {
+    console.error("Error getting scores:", error);
+  }
+}
+
+// Event listener for the form submit
+document.getElementById("score-form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const user = document.getElementById("user").value;
+  const score = parseInt(document.getElementById("score").value);
+
+  document.getElementById("user").value = "";
+  document.getElementById("score").value = "";
+
+  await addScore(user, score);
+  await getScores();
+});
+
+// Event listener for the refresh button
+document.getElementById("refresh-btn").addEventListener("click", async () => {
+  await getScores();
+});
+
+// Initialize the game and display scores on page load
+window.addEventListener("load", async () => {
+  try {
+    await createGame();
+    await getScores();
+  } catch (error) {
+    console.error("Error initializing the game:", error);
   }
 });
 
-window.addEventListener("load", () => {
-  document.getElementById("name").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("number").value = "";
+// Display scores on the page
+function displayScores(scores) {
+  const scoresContainer = document.getElementById("scores-container");
+  scoresContainer.innerHTML = "";
 
-  let existingData = JSON.parse(localStorage.getItem("savedData")) || [];
-
-  displaySavedData(existingData);
-});
-
-function displaySavedData(data) {
-  dataContainer.innerHTML = "";
-
-  data.forEach((entry) => {
-    const savedData = document.createElement("div");
-    savedData.innerHTML = `Name: ${entry.name}<br>Email: ${entry.email}<br>Number: ${entry.number}<br><hr>`;
-    dataContainer.appendChild(savedData);
+  scores.forEach((entry) => {
+    const scoreData = document.createElement("div");
+    scoreData.innerHTML = `User: ${entry.user}<br>Score: ${entry.score}<br><hr>`;
+    scoresContainer.appendChild(scoreData);
   });
 }
-
-const submitButton = document.getElementById('submitBtn');
-submitButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  const name = document.getElementById('name');
-  const score = document.getElementById('score');
-  const errDiv = document.getElementById('errDiv');
-  if (name.value !== '' && score.value !== '') {
-    submitData(name.value, score.value, gameID);
-    name.value = '';
-    score.value = '';
-    showingScores();
-    errDiv.style.display = 'none';
-  } else if (name.value === '' && score.value === '') {
-    errDiv.textContent = 'Name and Score fields are required';
-    errDiv.style.display = 'block';
-  } else if (name.value === '') {
-    errDiv.textContent = 'Please fill in your name';
-    errDiv.style.display = 'block';
-  } else if (score.value === '') {
-    errDiv.textContent = 'Please fill in your score';
-    errDiv.style.display = 'block';
-  }
-});
-
-const aLink = document.querySelector('a');
-aLink.addEventListener('click', (e) => {
-  e.preventDefault();
-  showingScores();
-});
-
-function askReload() {
-  return false;
-}
-
-window.onbeforeunload = askReload;
-window.onload = () => {
-  showingScores();
-  askReload();
-};
